9778c6baaeac18f1784fd29d2b77582fb0dfd213
Ermias5869 <ermiasamare8030@gmail.com>
2025-07-13 00:31:33 +0300

    feat: work course park

---
+++ app/admin/courses/create/page.tsx
@@ -0,0 +1,22 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <>
+      <div>
+        <Link
+          className={buttonVariants({
+            variant: "outline",
+            size: "icon",
+          })}
+          href="/admin/courses/create"
+        >
+          <ArrowLeft className="size-4" />
+        </Link>
+        <h1>Create Courses</h1>
+      </div>
+    </>
+  );
+}
--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,5 +1,18 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link className={buttonVariants()} href="admin/courses/create">
+          {" "}
+          Create Courses
+        </Link>
+      </div>
+      <div>Course soon..</div>
+    </>
+  );
 }
